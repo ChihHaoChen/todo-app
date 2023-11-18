6e65e7ceda17ddbee5e76d29adbc830d3c283a36
ChihHaoChen <chihhao.chen@linktivity.co.jp>
2023-11-18 21:36:41 +0900

    feat: add expo-local-auth

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -1,15 +1,43 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 import { Text, View } from "../../components/Themed";
+import * as LocalAuthentication from "expo-local-authentication";
+import { useCallback, useEffect, useState } from "react";
 
 export default function TabTwoScreen() {
+  const [isBiometricAvailable, setIsBiometricAvailable] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    compatibleCheck();
+  }, []);
+
+  const compatibleCheck = useCallback(async () => {
+    const isCompatible = await LocalAuthentication.hasHardwareAsync();
+    setIsBiometricAvailable(isCompatible);
+  }, []);
+
+  const onAuthenticate = useCallback(async () => {
+    try {
+      const result = await LocalAuthentication.authenticateAsync();
+      if (result.success) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>User Profile</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+      <Button onPress={onAuthenticate} title={"Login"} />
     </View>
   );
 }
